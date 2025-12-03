0127a68b8754d9d1b8204ef15631ee32dd5ab6f2
GansGanrnett <gansgarnett@gmail.com>
2025-12-03 23:12:08 +0500

    second commit

--- src/data/CardData.ts
+++ src/data/CardData.ts
@@ -3,6 +3,7 @@ import { ICardProps } from '../types/TCardProps';
 export const cards: ICardProps[] = Array.from({ length: 9 }).map((_, i) => ({
   id: i + 1,
   title: `Карта ${i + 1}`,
-  img: `https://picsum.photos/seed/card${i + 1}/600/400`,
+  // img: `https://picsum.photos/seed/card${i + 1}/600/400`,
+  img: `/cakes/img_${i+1}.jpg`,
   description: 'Нежный крем любого цвета на выбор, вафельная основа',
 }));
--- src/data/CarouselData.ts
+++ src/data/CarouselData.ts
@@ -1,10 +1,11 @@
 import { ICarouselItem } from '@/types/TCarouselItem';
 
-export const CarouselData: ICarouselItem[] = Array.from({ length: 9 }).map((_, i) => ({
+export const CarouselData: ICarouselItem[] = Array.from({ length: 3 }).map((_, i) => ({
   id: i + 1,
   title: '«Спасибо за прекрасные капкейки, все на высоте»',
   text: `«Никогда не выдумывайте отзывы ради красивого текста. Будьте честны: расскажите реальную историю или впечатление. Люди это ценят больше всего!»`,
-  img: `https://picsum.photos/seed/card${i + 1}/600/400`,
+  // img: `https://picsum.photos/seed/card${i + 1}/600/400`,
+  img: `/carousel/woman_${i+1}.jpg`,
 
   name: 'Иван Демидов',
   city: 'Краснодар',
